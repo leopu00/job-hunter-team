67e3c0e435bff4b5ee2af68e38866db526629a15
leopu00 <leopu00@gmail.com>
2026-04-08 18:01:18 +0200

    fix(cookie): make banner visible on all pages including login
    - Move CookieConsent outside LandingI18nProvider to ensure it renders everywhere
    - Make CookieConsent self-contained for language detection
    - Remove dependency on useLandingI18n context

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -14,6 +14,7 @@ import { LandingI18nProvider } from './components/landing/LandingI18n'
 import FadeInSection from './components/landing/FadeInSection'
 import JsonLd from './components/landing/JsonLd'
 import ScrollToTop from './components/landing/ScrollToTop'
+import CookieConsent from './components/landing/CookieConsent'
 
 function PageContent() {
   const params = useSearchParams()
@@ -45,7 +46,10 @@ function PageContent() {
 export default function LandingPage() {
   return (
     <Suspense>
-      <PageContent />
+      <>
+        <PageContent />
+        <CookieConsent />
+      </>
     </Suspense>
   )
 }
